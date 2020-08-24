de5c3872b38c33930010a5c426f9132ed0a9c7f5
Paul Yun <p.yun1994@gmail.com>
2020-08-24 20:04:23 +0000

    Automatically backed up by Learn

---
+++ bleh.js
@@ -0,0 +1,14 @@
+function find(array, criteriaFn){
+  let current = array;
+  let next = [];
+  while (current || current === 0){
+    if (criteriaFn(current)){
+      return current;
+	  }
+    if (Array.isArray(current)){
+      for (let i = 0; i < current.length; i ++){next.push(current[i])}
+    }
+    current = next.shift();
+  }
+  return null;
+}
